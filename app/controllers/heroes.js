c7b76ab5a985608eaa875b7dec0fb1523bf60a61
Dan33311 <60445863+Dan33311@users.noreply.github.com>
2020-06-02 10:34:39 -0500

    created heroes controller

---
+++ app/controllers/heroes.js
@@ -0,0 +1,140 @@
+const config = require('../../config.json');
+const Controller = function (TABLE) {
+    const express = require('express');
+    const router = express.Router();
+
+    const general = require("../utils/general")();
+    general.setDefaultDatabase(config.database.default);
+    let model = general.getDatabaseModel();
+
+
+    //{{SERVER}}/users/ 
+    //Lista todos los usuarios
+    router.get('/list', function (request, response) {
+        let validationToken = general.validateLogin(request);
+        if (true) {
+            model.getAll(TABLE)
+                .then((rows) => {
+                    response.send(rows);
+                }).catch((error) => {
+                    console.error(error);
+                    response.send(error);
+                });
+        } else {
+            response.send({ error: 'No se ha enviado un token', message: validationToken.message });
+        }
+
+    });
+
+    //{{SERVER}}/users/id 
+    //Trae un usuario por ID
+    router.get('/:id', function (request, response) {
+        let id = request.params.id;
+        let validationToken = general.validateLogin(request);
+        if (true) {
+            model.getById(TABLE, id)
+                .then((row) => {
+                    response.send(row);
+                }).catch((error) => {
+                    console.error(error);
+                    response.send(error);
+                });
+        } else {
+            response.send({ error: 'No se ha enviado un token' });
+        }
+    });
+
+    //{{SERVER}}/users/
+    //Crea un usuario
+    router.post('/', function (request, response) {
+        let validationToken = general.validateLogin(request);
+        if (true) {
+            model.create(TABLE, request.body)
+                .then((object) => {
+                    response.send(object)
+                }).catch((error) => {
+                    console.error(error);
+                    response.send(error);
+                });
+        } else {
+            response.send({ error: 'No se ha enviado un token' });
+        }
+    });
+
+    //{{SERVER}}/users/:id
+    //Edita un usuario
+    router.put('/:id', function (request, response) {
+        let id = request.params.id;
+        let validationToken = general.validateLogin(request);
+        if (true) {
+            model.update(TABLE, request.body, id)
+                .then((row) => {
+                    response.send(row);
+                }).catch((error) => {
+                    console.error(error);
+                    response.send(error);
+                });
+        } else {
+            response.send({ error: 'No se ha enviado un token' });
+        }
+
+    });
+
+
+    //{{SERVER}}/users/id
+    //Elimina un usuario
+    router.delete('/:id', function (request, response) {
+        let id = request.params.id;
+        let validationToken = general.validateLogin(request);
+        if (true) {
+            model.delete(TABLE, id)
+                .then((message) => {
+                    response.send(message);
+                }).catch((error) => {
+                    console.error(error);
+                    response.send(error);
+                });
+        } else {
+            response.send({ error: 'No se ha enviado un token' });
+        }
+    });
+
+
+    //{{SERVER}}/users/delete_users
+    //Limpiar tabla
+    router.get('/option/clean', function (request, response) {
+        let validationToken = general.validateLogin(request);
+        if (true) {
+            model.clean(TABLE)
+                .then((message) => {
+                    response.send(message)
+                }).catch((error) => {
+                    console.error(error);
+                    response.send(error);
+                });
+        } else {
+            response.send({ error: 'No se ha enviado un token' });
+        }
+    });
+
+    //{{SERVER}}/users/create_users
+    router.post('/option/initialize', function (request, response) {
+        let validationToken = general.validateLogin(request);
+        if (true) {
+            model.initialize(TABLE, request.body)
+                .then((message) => {
+                    response.send(message)
+                }).catch((error) => {
+                    console.error(error);
+                    response.send(error);
+                });
+        } else {
+            response.send({ error: 'No se ha enviado un token' });
+        }
+    });
+
+
+    return router;
+};
+
+module.exports = Controller;
--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const config = require('./config.json');
 //const config2 = require('./app/utils/config');
 //son equivalentes
 const port = process.env.PORT ? process.env.PORT : config.app.port ? config.app.port : 3003;
-const bind = process.env.BIND ? process.env.BIND : config.app.bind ? config.app.bind : '127.0.0.1' ;
+const bind = process.env.BIND ? process.env.BIND : config.app.bind ? config.app.bind : '127.0.0.1';
 //const port = process.env.PORT || config.app.port || 3003;
 
 //URL Encode support for POST, PUT methods
@@ -18,30 +18,35 @@ let usersController = require('./app/utils/controller')('users');
 let badge_controller = require('./app/utils/controller')('badge');
 let challengesController = require('./app/utils/controller')('challenge');
 let trainingsController = require('./app/utils/controller')('training');
-let advancesController = require('./app/utils/controller')('advance');   // New
+let advancesController = require('./app/utils/controller')('advance');  
 
 //Relación tablas
 let badge_user_controller = require('./app/utils/controller')('badge_user');
 
+
+let heroController = require('./app/controllers/heroes')('heroes');  // new
+app.use('/heroes', heroController);
+
+
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use('/advance', advancesController);  // New
-app.use('/users', usersController);
-app.use('/training', trainingsController);
-app.use('/challenge', challengesController);
-app.use('/badge', badge_controller);
-app.use('/login', loginController);
-app.use('/badge_user', badge_user_controller);
+// app.use('/advance', advancesController);  // New
+// app.use('/users', usersController);
+// app.use('/training', trainingsController);
+// app.use('/challenge', challengesController);
+// app.use('/badge', badge_controller);
+// app.use('/login', loginController);
+// app.use('/badge_user', badge_user_controller);
 
-app.use('/', (request, response)=>{
-    response.send('Hi there, now in API ' + config.app.name);
-});
+// app.use('/', (request, response) => {
+//     response.send('Hi there, now in API ' + config.app.name);
+// });
 
-app.listen(port, function(){
+app.listen(port, function () {
     console.log('***********************');
-    console.log("App: "+config.app.name);
-    console.log('Running: '+ config.app.bind+':'+config.app.port);
+    console.log("App: " + config.app.name);
+    console.log('Running: ' + config.app.bind + ':' + config.app.port);
     console.log('***********************');
 });
 
